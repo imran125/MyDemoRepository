7d32d83141528b5a9d39fedecfd361bfb15edd59
imrankhawaja <imran.aleem.khawaja@gmail.com>
2020-12-05 23:44:54 +0000

    This is my third modified commit on git

--- Demo1.java
+++ Demo1.java
@@ -1,7 +1,8 @@
-class Demo1
+public class Demo1
 {
 
-	public static void main(string args[]) {
-	System.out.println("This is Java class Demo1")
+	public static void main (String args[]) {
+	System.out.println("This is Java class Demo1");
+	System.out.println("This is second line from Java class Demo1");
 	}
-}	
+} 	
---
+++ Demo2.java
@@ -0,0 +1,8 @@
+class Demo2
+{
+
+	public static void main(string args[]) {
+	System.out.println("This is Java class Demo2");
+	
+	}
+} 	
