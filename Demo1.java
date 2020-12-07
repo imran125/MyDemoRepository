25d303c11bb4edaf54af29344c75393148c5033a
imrankhawaja <imran.aleem.khawaja@gmail.com>
2020-12-07 08:40:50 +0000

    This is my correction commit

--- Demo1.java
+++ Demo1.java
@@ -1,10 +1,9 @@
-class Demo1
+public class Demo1
 { 
 
 	public static void main (String args[]) {
 	System.out.println("This is Java Demo1");
-	System.out.println("This is 2nd line from Java class Demo1")
-
+	System.out.println("This is 2nd line from Java class Demo1");
 	}
 
 } 	
