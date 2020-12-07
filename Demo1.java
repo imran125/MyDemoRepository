0aa9e72c3b23a69ce1da260ed6c63a0734ac8d9c
imrankhawaja <imran.aleem.khawaja@gmail.com>
2020-12-07 23:27:45 +0000

    This is my assertive commit on git

--- Demo1.java
+++ Demo1.java
@@ -1,7 +1,7 @@
 public class Demo1
 { 
 
-	public static void main (String args[]) {
+	public static void main(String args[]){
 	System.out.println("This is Java Demo1");
 	System.out.println("This is 2nd line from Java class Demo1");
 	}
