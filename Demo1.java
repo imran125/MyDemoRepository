abbb713e3cfc30457e02b6a612e2e2bf3e2bed96
imrankhawaja <imran.aleem.khawaja@gmail.com>
2020-12-07 07:57:30 +0000

    Deleted all files

--- Demo1.java
+++
@@ -1,8 +0,0 @@
-public class Demo1
-{
-
-	public static void main (String args[]) {
-	System.out.println("This is Java class Demo1");
-	System.out.println("This is second line from Java class Demo1");
-	}
-} 	
--- Demo2.java
+++
@@ -1,8 +0,0 @@
-class Demo2
-{
-
-	public static void main(string args[]) {
-	System.out.println("This is Java class Demo2");
-	
-	}
-} 	
